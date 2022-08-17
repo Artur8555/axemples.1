4ed9289ba0cc7f32617ed200eba4d1e01c7ad316
Arturo <artur.barbershop@gmail.com>
2022-08-17 17:35:47 +0300

    math2

---
+++ example.helloworld/example01.math/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = 32;// целые числа
+int numberB = 32;
+Console.WriteLine(numberA + numberB);
+double numberC = 32;// целочислиые  числа
+double numberD = 3;
+Console.WriteLine(numberC / numberD);
