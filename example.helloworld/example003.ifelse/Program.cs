4e5a03a301f8aca4b0c035c0e4ede9bc58e3d877
Arturo <artur.barbershop@gmail.com>
2022-08-17 18:03:49 +0300

    ifelse

---
+++ example.helloworld/example003.ifelse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ввидиде имя пользователя :");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "Маша")//распознавание шрифта
+{
+    Console.WriteLine(" УРА ЭТО ПРИВЕТ МАША!!");
+}
+{
+    Console.WriteLine("ПРИВЕТ !! :");
+    Console.WriteLine(userName);
+}
