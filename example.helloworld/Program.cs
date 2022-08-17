8b123cf8c84470026e6a7e6fc6f6588675b13655
Arturo <artur.barbershop@gmail.com>
2022-08-17 17:13:08 +0300

    2 file new

--- example.helloworld/Program.cs
+++ example.helloworld/Program.cs
@@ -1,2 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
-//выводит текст в консоль
+﻿Console.WriteLine("Hello, World!");//выводит текст в консоль
+Console.Write("input your name : "); // в одну строку
+string userName = Console.ReadLine();//считывания данных
+Console.WriteLine("Hello, " + userName);//выводит на новую строку
