b4021034a6dc6c0c182e030c82114dd3546af22c
Arturo <artur.barbershop@gmail.com>
2022-08-17 18:17:26 +0300

    if

---
+++ example.helloworld/example004.besse/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 21;
+int c = 24;
+int f = 25;
+int d = 27;
+int r = 200;
+int t = 201;
+int w = 205;
+
+int max = a;
+
+if (a > max) max = a;
+if (c > max) max = c;
+if (f > max) max = f;
+if (d > max) max = d;
+if (r > max) max = r;
+if (t > max) max = t;
+if (w > max) max = w;
+Console.Write("max = ");
+Console.WriteLine(max);
