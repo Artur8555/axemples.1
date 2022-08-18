4bb508a8a04bd0d42d8ff16cf96585b7cb7ae150
Arturo <artur.barbershop@gmail.com>
2022-08-18 18:20:56 +0300

    posision index 2

--- example.helloworld/example10.array/Program.cs
+++ example.helloworld/example10.array/Program.cs
@@ -1,4 +1,4 @@
-﻿int[] array = { 1, 9, 6, 5, 7, 8, 4, 2, 7 };
+﻿int[] array = { 1, 9, 4, 6, 4, 5, 7, 8, 4, 2, 7 };
 int n = array.Length;
 int find = 4;//найти позицию числа по  индаксу 
 int index = 0;
@@ -8,6 +8,7 @@ while (index < n)
     if (array[index] == find)
     {
         Console.WriteLine(index);
+        break;// при первом нахождении остоновит итерацию
     }
     index++;
 
